277d32b7e280dad9bfe44e36c5338b6b17869e02
kingoffullmoon <kingoffullmoon02@gmail.com>
2023-08-07 14:08:14 +0700

    update

--- migrations/1_token_deploy_contracts.js
+++ migrations/1_token_deploy_contracts.js
@@ -1,9 +1,9 @@
 
 const BRE = artifacts.require("BRE");
-//const GRBProxy = artifacts.require("GRBProxy");
+const BreezyGateKeeper = artifacts.require("BreezyGateKeeper");
 
 module.exports = function(deployer) {
-    deployer.deploy(BRE, "21000000000000000000000000", "10000000000000000000000").then(function() {
-        
+    deployer.deploy(BRE, "21000000000000000000000000", "21000000000000000000000000").then(function() {
     });
+    deployer.deploy(BreezyGateKeeper, 1, [""], [""], "");
 };
