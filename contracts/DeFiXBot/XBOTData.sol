0cee4bdb0d31993b0f8694110a8d0a7fee25ec4c
izanagi88 <shiroizanagi88@gmail.com>
2023-10-13 11:49:42 +0700

    update

---
+++ contracts/DeFiXBot/XBOTData.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.6.12;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+interface IXBOT {
+    function buyPrice() external view returns (uint256);
+    function sellPrice() external view returns (uint256);
+    function totalSupply() external view returns (uint256);
+    function getETHBalance() external view returns (uint256);
+    function myDividends(bool _includeReferralBonus, address _customerAddress) external view returns (uint256);
+}
+
+contract XBOTData is Ownable{
+
+    IXBOT public XBOTContract;
+
+    constructor(IXBOT xbotContractAddress) {
+        XBOTContract = xbotContractAddress;
+    }
+
+    function setXbotContract(IXBOT xbotContractAddress) public onlyOwner {
+        XBOTContract = xbotContractAddress;
+    }
+
+    function getData(bool includeReferralBonus, address user) public view returns (
+        uint256 buyPrice,
+        uint256 sellPrice,
+        uint256 totalSupply,
+        uint256 ethBalanceUser,
+        uint256 myDividends
+    ) {
+        buyPrice = XBOTContract.buyPrice();
+        sellPrice = XBOTContract.sellPrice();
+        totalSupply = XBOTContract.totalSupply();
+        ethBalanceUser = user.balance;
+        myDividends = XBOTContract.myDividends(includeReferralBonus, user);
+    }
+}
--- migrations/1_token_deploy_contracts.js
+++ migrations/1_token_deploy_contracts.js
@@ -6,7 +6,13 @@ const BreezyFarmingFund = artifacts.require("BreezyFarmingFund");
 const BreezyNFTLPLock = artifacts.require("BreezyNFTLPLock");
 const XBOT = artifacts.require("XBOT");
 const XBOTTreasury = artifacts.require("XBOTTreasury");
+const XBOTData = artifacts.require("XBOTData");
 
+const xbotContract = {
+    arbitrumGoerli: "0xa093eaf146173438dd11bc684209744b84d9218d",
+    sepolia: "0xF55D10BaeA959a54AfB9F4e4E05C52cC8CC8EcF1",
+    base: "",
+} 
 
 module.exports = function(deployer) {
 //    deployer.deploy(BRE, "21000000000000000000000000", "21000000000000000000000000").then(function() {
@@ -15,6 +21,7 @@ module.exports = function(deployer) {
 //    deployer.deploy(BreezyAirdropAndDevFund);
 //    deployer.deploy(BreezyFarmingFund);
 //    deployer.deploy(BreezyNFTLPLock);
-    deployer.deploy(XBOT);
-    deployer.deploy(XBOTTreasury);
+    // deployer.deploy(XBOT);
+    // deployer.deploy(XBOTTreasury);
+    deployer.deploy(XBOTData, xbotContract.sepolia);
 };
