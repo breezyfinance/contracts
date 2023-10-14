c9732de29a0855351f051b5e677bffd64e7b240d
Shiro Izanagi <shiroizanagi88@gmail.com>
2023-10-14 10:25:58 +0700

    update

--- contracts/DeFiXBot/XBOTData.sol
+++ contracts/DeFiXBot/XBOTData.sol
@@ -6,6 +6,7 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 interface IXBOT {
     function buyPrice() external view returns (uint256);
     function sellPrice() external view returns (uint256);
+    function balanceOf(address user) external view returns (uint256);
     function totalSupply() external view returns (uint256);
     function getETHBalance() external view returns (uint256);
     function myDividends(bool _includeReferralBonus, address _customerAddress) external view returns (uint256);
@@ -26,12 +27,14 @@ contract XBOTData is Ownable{
     function getData(bool includeReferralBonus, address user) public view returns (
         uint256 buyPrice,
         uint256 sellPrice,
+        uint256 balanceUser,
         uint256 totalSupply,
         uint256 ethBalanceUser,
         uint256 myDividends
     ) {
         buyPrice = XBOTContract.buyPrice();
         sellPrice = XBOTContract.sellPrice();
+        balanceUser = XBOTContract.balanceOf(user);
         totalSupply = XBOTContract.totalSupply();
         ethBalanceUser = user.balance;
         myDividends = XBOTContract.myDividends(includeReferralBonus, user);
